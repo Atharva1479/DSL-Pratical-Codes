4fd186d9c883865a392c0c62a5f2396f0cee40da
Atharva1479 <76987608+Atharva1479@users.noreply.github.com>
2022-05-22 13:01:00 +0530

    Add files via upload

--- B5.cpp
+++ B5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct node // Node Declaration
 {
 	string label;
-	// char label[10];
+	//char label[10];
 	int ch_count;
 	struct node *child[10];
 } * root;
@@ -66,8 +66,7 @@ void GT::display(node *r1)
 			cout << "\nSections : ";
 			for (j = 0; j < r1->child[i]->ch_count; j++)
 			{
-				cout << "\n"
-					 << r1->child[i]->child[j]->label;
+				cout << "\n" << r1->child[i]->child[j]->label;
 			}
 		}
 	}
@@ -97,84 +96,9 @@ int main()
 			break;
 		case 3:
 			cout << "Thanks for using this program!!!";
-			//exit(1);
 		default:
 			cout << "Wrong choice!!!" << endl;
 		}
 	}
 	return 0;
-}
-
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-using namespace std;
-struct node
-{
- int data;
- node *left, *right;
-};
-class tree
-{
-public:
- node *create()
- {
- node *p;
- int x;
- p = new node;
- cout << "ENTER DATA-: ";
- cin >> x;
- p->data = x;
- if (x == -1)
- {
- return NULL;
- }
- cout << "ENTER LEFT CHILD OF " << x << "- ";
- p->left = create();
- cout << "ENTER RIGHT CHILD OF " << x << "- ";
- p->right = create();
- return p;
- }
- void preorder(node *t)
- {
- if (t != NULL)
- {
- cout << t->data;
- preorder(t->left);
- preorder(t->right);
- }
- }
- void postorder(node *t)
- {
- if (t != NULL)
- {
- postorder(t->left);
- postorder(t->right);
- cout << t->data;
- }
- }
- void inorder(node *t)
- {
- if (t != NULL)
- {
- inorder(t->left);
- cout << t->data;
- inorder(t->right);
- }
- }
-};
-int main()
-{
- tree t;
- node *root;
- root = t.create();
- cout << "PREORDER SEQUENCE-:";
- t.preorder(root);
- cout << endl;
- cout << "POSTORDER SEQUENCE-:";
- t.postorder(root);
- cout << endl;
- cout << "INORDER SEQUENCE-:";
- t.inorder(root);
- return 0;
 }
